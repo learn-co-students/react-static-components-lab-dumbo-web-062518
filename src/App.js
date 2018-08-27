c23f2177ec9fdcb5a0cb2619ddb5255e47043912
storres8 <stevetorres7562@gmail.com>
2018-08-26 21:47:45 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import { FordQuoteComponent } from './FordQuoteComponent'
-import MouseComponent from '../MouseComponent'
+import FordQuoteComponent  from './FordQuoteComponent'
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
   render() {
@@ -15,5 +15,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default App;
